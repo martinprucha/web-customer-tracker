211a0a0c263c25c1991903e4384261c575fd8b62
Martin Prucha <prucha.martin@gmail.com>
2018-04-18 15:49:57 +0200

    Lecture 299 completed

--- src/com/luv2code/springdemo/controller/CustomerController.java
+++ src/com/luv2code/springdemo/controller/CustomerController.java
@@ -6,27 +6,49 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.luv2code.springdemo.dao.CustomerDAO;
 import com.luv2code.springdemo.entity.Customer;
+import com.luv2code.springdemo.service.CustomerService;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
-	
+
 	@Autowired
-	private CustomerDAO customerDAO;
+	private CustomerService customerService;
 	
 	@GetMapping("/list")
 	public String listCustomers(Model theModel) {
 		
-		// get customers from DAO
-		List<Customer> theCustomers = customerDAO.getCustomers();
+		// get customers from the service
+		List<Customer> theCustomers = customerService.getCustomers();
 		
 		// add customers to the model
 		theModel.addAttribute("customers", theCustomers);
 		
 		return "list-customers";
+	}
+	
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+		
+		// create model attribute to bind form data
+		Customer theCustomer = new Customer();
+		
+		theModel.addAttribute(theCustomer);
+		
+		return "customer-form";
+	}
+	
+	@PostMapping("/saveCustomer")
+	public String saveCustomer(@ModelAttribute("customer") Customer theCustomer) {
+		
+		// save the customer
+		customerService.saveCustomer(theCustomer);
+		
+		return "redirect:/customer/list";
 	}
 }
